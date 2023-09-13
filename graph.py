9a56d0fb98ad623b2d3fbfa9f185de7f1b242d98
fateme-alk <fateme.alk9@gmail.com>
2023-09-13 07:38:30 +0330

    change the graph's DS implementation just with nested dictionaries as simpler form

--- graph.py
+++ graph.py
@@ -14,21 +14,14 @@ class Graph:
 
     def add_vertex(self, name, coordinate):
         v = Vertex(name, coordinate)
-        self.adj_list[v] = []
+        self.adj_list[v] = {}
         return v
 
     
     def add_edge(self, end_p1, end_p2):
         e = Edge(end_p1, end_p2)
-        self.adj_list[end_p1].append({
-            'adjacent_vertex': end_p2,
-            'weight': e.weight
-        })
-        self.adj_list[end_p2].append({
-            'adjacent_vertex': end_p1,
-            'weight': e.weight
-        })
-
+        self.adj_list[end_p1][end_p2] = e.weight
+        self.adj_list[end_p2][end_p1] = e.weight
 
     def vertex_exists(self, vertex):
         if vertex in self.adj_list:
