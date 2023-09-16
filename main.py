791fe1f80b7947a08063ea0ad79fce477f34a7d2
fateme-alk <fateme.alk9@gmail.com>
2023-09-16 19:55:53 +0330

    add after_this_request decorator to solve the CORS Error

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, after_this_request
 
 
 app = Flask(__name__)
 @app.route('/navigator', methods=['GET'])
 def route_handler():
-    pass
+    @after_this_request
+    def add_header(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
 if __name__ == '__main__':
     app.run()
